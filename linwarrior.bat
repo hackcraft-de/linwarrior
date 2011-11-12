0759c710954d1f21c68b7e82a581e8dad1c18d99
Benjamin Pickhardt <benjamin.pickhardt@udo.edu>
2011-11-12 20:47:32 +0100

    Project: Added module launcher.

--- linwarrior.bat
+++ linwarrior.bat
@@ -1,4 +1,4 @@
-rem dist\linwarrior -m 2 -r 640x400
-rem dist\linwarrior -m 0 -r 1680x1050
-rem dist\linwarrior -m 0 -r 1152x864
-dist\linwarrior -m 0 -r 1680x1050
+rem dist\linwarrior game -m 2 -r 640x400
+rem dist\linwarrior game -m 0 -r 1680x1050
+rem dist\linwarrior game -m 0 -r 1152x864
+dist\linwarrior game -m 0 -r 1680x1050
--- source/de/hackcraft/main.cpp
+++ source/de/hackcraft/main.cpp
@@ -16,20 +16,42 @@ using namespace std;
 
 #include "de/hackcraft/game/GameMain.h"
 
-#ifdef wonly_WIN32
+static int test(int argc, char **args) {
+    cout << "Arguments for test module:\n";
+    for (int i = 0; i < argc; i++) {
+        cout << args[i] << "\n";
+    }
+    return 0;
+}
 
-extern "C" int __stdcall WinMain(void* hInstance, void* hPrevInstance, char* lpCmdLine, int nCmdShow) {
-    int argc = 2;
-    char *args[] = {"Nothing", "Nothing", "Nothing"};
-#else
+//#ifdef wonly_WIN32
+//extern "C" int __stdcall WinMain(void* hInstance, void* hPrevInstance, char* lpCmdLine, int nCmdShow) {
+//    int argc = 2;
+//    char *args[] = {"Nothing", "Nothing", "Nothing"};
+//#else
+//#endif
 
 int main(int argc, char **args) {
-#endif
     
     try {
-        return (new GameMain())->run(argc, args);
+        string subprogram = (argc >= 2) ? string(args[1]) : string("");
+        
+        if (subprogram == "game") {
+            return (new GameMain())->run(argc-1, &args[1]);
+            
+        } else if (subprogram == "test") {
+            return test(argc-1, &args[1]);
+            
+        } else {
+            cout << "Please specify module to run: <program> <module> [parameters]\n";
+            cout << "  game   Run game module\n";
+            cout << "  test   Run test module\n";
+            return 0;
+        }
+        
     } catch (char* s) {
         cout << "Fatal exception caught:\n" << s << endl;
+        
     } catch (const char* s) {
         cout << "Fatal exception caught:\n" << s << endl;
     }
