1f691ac6a3a4cb8c88f726f6b965e66f5e053ca8
Benjamin Pickhardt <benjamin.pickhardt@udo.edu>
2012-04-29 17:25:49 +0200

    Subsystem-Interface: Updating format and comments.

--- source/de/hackcraft/world/Subsystem.h
+++ source/de/hackcraft/world/Subsystem.h
@@ -15,7 +15,8 @@ class Subsystem {
 
 public: // Interaction
     /**
-     *
+     * Limit position to accessible areas and returns impact depth.
+     * 
      * @param worldpos The particle position to be constrained given in world coordinates.
      * @param radius The size of the particle in terms of a radius.
      * @return Zero if there was no collision, else the maximum impact depth.
@@ -23,23 +24,31 @@ public: // Interaction
     virtual float constrainParticle(Entity* ex, float* worldpos, float radius) { return 0.0f; };
     
 public: // Frame cycle
-    /// Advance simulation time for one frame.
+    /** Advance simulation time for one frame. */
     virtual void advanceTime(int deltamsec) {};
-    /// Re-build spatial clustering of objects.
+    
+    /** Re-build spatial clustering of objects. */
     virtual void clusterObjects() {};
-    /// Deliver overdue messages to objects.
+    
+    /** Deliver overdue messages to objects. */
     virtual void dispatchMessages() {};
-    /// Let all objects process input, adjust pose and calculate physics.
+    
+    /** Let all objects process input, adjust pose and calculate physics. */
     virtual void animateObjects() {};
-    /// Let all objects calculate transformation matrices etc.
+    
+    /** Let all objects calculate transformation matrices etc. */
     virtual void transformObjects() {};
-    /// Setup structures for rendering
+    
+    /** Setup structures for rendering */
     virtual void setupView(float* pos, float* ori) {};
-    /// Draws background (skybox).
+    
+    /** Draws background (skybox). */
     virtual bool drawBack() { return false; };
-    /// Draw all Object's solid surfaces (calls their drawSolid method).
+    
+    /** Draw all Object's solid surfaces (calls their drawSolid method). */
     virtual void drawSolid() {};
-    /// Draw all Object's effects (calls their drawEffect method).
+    
+    /** Draw all Object's effects (calls their drawEffect method). */
     virtual void drawEffect() {};
 };
 
