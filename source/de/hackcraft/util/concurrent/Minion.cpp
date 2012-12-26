cd40b35db31e9889ba1a11a23dfe6a08707f486d
Benjamin Pickhardt <benjamin.pickhardt@udo.edu>
2012-12-26 09:31:43 +0100

    Minion: Fixing logger name and adding vertical spacing.

--- source/de/hackcraft/util/concurrent/Minion.cpp
+++ source/de/hackcraft/util/concurrent/Minion.cpp
@@ -7,24 +7,29 @@
 #include "de/hackcraft/util/concurrent/Semaphore.h"
 
 
-Logger* Minion::logger = Logger::getLogger("de.hackcraft.game.Minion");
+Logger* Minion::logger = Logger::getLogger("de.hackcraft.util.concurrent.Minion");
 
 
 Minion::Minion(Semaphore* jobMutex, std::queue<Runnable*>* jobQueue) {
+    
     this->jobMutex = jobMutex;
     this->jobQueue = jobQueue;
 }
 
 
 void Minion::run() {
-    unsigned long id = (unsigned long) this;//SDL_ThreadID();
+    
+    unsigned long id = (unsigned long) this;//SDL_ThreadID();    
     logger->info() << "Minion " << id << " at your service!\n";
+    
     Runnable* job = (Runnable*) this;
     bool done = false;
 
     while (!done) {
         // Grab a new job.
+        
         Runnable* nextjob = NULL;
+        
         jobMutex->acquire();
         {
             if (!jobQueue->empty()) {
@@ -33,20 +38,27 @@ void Minion::run() {
             } // else look at secondary jobs.
         }
         jobMutex->release();
+        
         // Work on job if any available.
+        
         if (nextjob != NULL) {
+            
             logger->debug() << "Minion " << id << " is fulfilling your wish!\n";
             job = nextjob;
             job->run();
             int result = 0;
             logger->debug() << "Minion " << id << " job is finished with result " << result << "!\n";
+            
         } else if (job != NULL && nextjob == NULL) {
+            
             // Wait for a job to do.
             logger->debug() << "Minion " << id << " is awaiting your command!\n";
             job = nextjob;
             Thread::sleep(10);
+            
         } else {
             Thread::sleep(10);
         }
+        
     }
 }
