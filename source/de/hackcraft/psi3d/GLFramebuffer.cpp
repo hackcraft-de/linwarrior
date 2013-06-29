c8e6a63c00fe2ae7a01cd00ef20c72369ff98889
Benjamin Pickhardt <benjamin.pickhardt@udo.edu>
2013-06-29 16:46:38 +0200

    GLFramebuffer: Adding initial class version for offscreen rendering.

---
+++ source/de/hackcraft/psi3d/GLFramebuffer.cpp
@@ -0,0 +1,102 @@
+#include "GLFramebuffer.h"
+
+#include "de/hackcraft/opengl/GL.h"
+
+
+unsigned int GLFramebuffer::getFramebuffer() {
+    return framebuffer;
+}
+
+
+unsigned int GLFramebuffer::getColorbuffer() {
+    return colorbuffer;
+}
+
+
+unsigned int GLFramebuffer::getDepthbuffer() {
+    return depthbuffer;
+}
+
+
+unsigned int GLFramebuffer::getWidth() {
+    return width;
+}
+
+
+unsigned int GLFramebuffer::getHeight() {
+    return height;
+}
+
+
+void GLFramebuffer::initBuffers(int w, int h) {
+
+    this->width = w;
+    this->height = h;
+
+    unsigned int colorbuffer;
+    unsigned int depthbuffer;
+    unsigned int framebuffer;
+
+    void* undefinedTexels = NULL;
+
+    // Generate and initialize color-buffer.
+    GL::glGenTextures(1, &colorbuffer);
+    GL::glBindTexture(GL_TEXTURE_2D, colorbuffer);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    GL::glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_BGRA, GL_UNSIGNED_BYTE, undefinedTexels);
+    GL::glGenerateMipmapEXT(GL_TEXTURE_2D);
+
+    // Generate and initialize depth-buffer.
+    GL::glGenTextures(1, &depthbuffer);
+    GL::glBindTexture(GL_TEXTURE_2D, depthbuffer);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_DEPTH_TEXTURE_MODE, GL_INTENSITY);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_R_TO_TEXTURE);
+    GL::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+    GL::glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, w, h, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, undefinedTexels);
+    GL::glGenerateMipmapEXT(GL_TEXTURE_2D);
+
+    // Generate frame-buffer and attach color- and depth-buffer.
+    GL::glGenFramebuffersEXT(1, &framebuffer);
+    GL::glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, framebuffer);
+    GL::glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, colorbuffer, 0/*mipmap level*/);
+    GL::glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_TEXTURE_2D, depthbuffer, 0/*mipmap level*/);
+
+    this->colorbuffer = colorbuffer;
+    this->depthbuffer = depthbuffer;
+    this->framebuffer = framebuffer;
+    
+    //Does the GPU support current FBO configuration?
+    GL::GLenum status;
+    status = GL::glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+    if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+        throw "Could not initialize frame-buffer.";
+    }
+}
+
+
+void GLFramebuffer::bindBuffers() {
+    GL::glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, framebuffer);
+}
+
+
+void GLFramebuffer::unbindBuffers() {
+    GL::glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+}
+
+
+void GLFramebuffer::cleanBuffers() {
+    //Delete resources
+    GL::glDeleteTextures(1, &colorbuffer);
+    GL::glDeleteTextures(1, &depthbuffer);
+    //Bind 0, which means render to back buffer, as a result, fb is unbound
+    GL::glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+    GL::glDeleteFramebuffersEXT(1, &framebuffer);
+}
+
---
+++ source/de/hackcraft/psi3d/GLFramebuffer.h
@@ -0,0 +1,53 @@
+/* 
+ * File:   GLFramebuffer.h
+ * Author: Benjamin Pickhardt
+ * Home:   hackcraft.de
+ *
+ * Created on June 29, 2013, 4:24 PM
+ */
+
+#ifndef GLFRAMEBUFFER_H
+#define	GLFRAMEBUFFER_H
+
+/**
+ * Framebuffer-Object for offscreen and texture rendering according to:
+ * https://www.opengl.org/wiki/Framebuffer_Object_Examples
+ * 
+ * May change because it's said to deliver higher performance
+ * to recycle one frame-buffer and just exchange the textures.
+ * That is given they have the same size - which may/not be the case.
+ * 
+ * Basically it just works by creating textures for buffers and binding
+ * them to the frame-buffer-object which is just a container for the bindings.
+ * Once that is done the frame-buffer-object can be bound and unbound
+ * to enable and disable rendering to that one instead of the usual
+ * screen-frame-buffer.
+ * 
+ * There isn't much configuration of the buffer properties right now -
+ * just using sensible usual quality values.
+ */
+class GLFramebuffer {
+private:
+    unsigned int width;
+    unsigned int height;
+    unsigned int framebuffer;
+    unsigned int colorbuffer;
+    unsigned int depthbuffer;
+    
+public:
+    unsigned int getFramebuffer();
+
+    unsigned int getColorbuffer();
+    unsigned int getDepthbuffer();
+    
+    unsigned int getWidth();
+    unsigned int getHeight();
+    
+    void initBuffers(int w, int h);
+    void bindBuffers();
+    void unbindBuffers();
+    void cleanBuffers();
+};
+
+#endif	/* GLFRAMEBUFFER_H */
+
